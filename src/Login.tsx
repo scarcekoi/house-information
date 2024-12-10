8ac0278b6165c85c6efea394158d3b8e6e148a7a
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-10 06:54:04 -0500

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import Login from './Lgn.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('login')!).render(
   <StrictMode>
     <Login />
   </StrictMode>,
