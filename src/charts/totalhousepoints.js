3828bf663a6bfe67cd36f38c04a4b69aa1eb35b3
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-17 07:03:39 -0500

    Update totalhousepoints.js

--- src/charts/totalhousepoints.js
+++ src/charts/totalhousepoints.js
@@ -1,15 +1,25 @@
 import Chart from 'chart.js/auto'
 
 (async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  const data = {
+  labels: [
+    'Baldwin',
+    'Sotomayor',
+    'Mandela',
+    'Truth'
+  ],
+  datasets: [{
+    label: 'Total House Points',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(210, 15, 57)',
+      'rgb(30, 102, 245)',
+      'rgb(64, 160, 43)',
+      'rgb(223, 142, 29)'
+    ],
+    hoverOffset: 4
+  }]
+  };
 
   new Chart(
     document.getElementById('acquisitions'),
@@ -29,23 +39,3 @@ import Chart from 'chart.js/auto'
       data: data,
     }
   );
-
-  const data = {
-  labels: [
-    'Baldwin',
-    'Sotomayor',
-    'Mandela',
-    'Truth'
-  ],
-  datasets: [{
-    label: 'Total House Points',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(210, 15, 57)',
-      'rgb(30, 102, 245)',
-      'rgb(64, 160, 43)',
-      'rgb(223, 142, 29)'
-    ],
-    hoverOffset: 4
-  }]
-};
