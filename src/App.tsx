8b2053a0c9567c8d7d155aa57c75ebaef51e9575
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-19 07:10:49 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import TotalHousePoints from './charts/totalhousepoints.tsx';
-import Footer from './Footer.tsx';
 
 interface Counters {
   Baldwin: number;
