e7eca3c38b3f681406c38fa560d455920034de83
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-19 07:04:56 -0500

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,7 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <p>&copy; Hand-coded with <i className="fa-solid fa-heart" title="love"></i> by Thelonious</p>
+      <p>Hand-coded with <i className="fa-solid fa-heart" title="love"></i> by Thelonious</p>
     </div>
   );
 };
