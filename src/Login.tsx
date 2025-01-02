4d36e2a0408110229a06b9f34e9613bdcc45e94b
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-01 22:42:48 -0500

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Login from './Lgn.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Login from './Lgn.tsx';
+import './index.css';
+
+const handleLoginSuccess = () => {
+  console.log('Login was successful!');
+};
 
 createRoot(document.getElementById('login')!).render(
   <StrictMode>
-    <Login />
+    <Login onLoginSuccess={handleLoginSuccess} />
   </StrictMode>,
-)
+);
