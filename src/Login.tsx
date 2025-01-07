8f494dff93b3193450178f17ae7710e477d9c4e6
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-06 19:18:23 -0500

    OMG

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './Lgn.tsx'
+import Login from './pages/Lgn.tsx'
 import './index.css'
 
 createRoot(document.getElementById('login')!).render(
