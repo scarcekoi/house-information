75c3f2dc0bb0bea0c099c7c1761591acde8e32a3
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-06 19:44:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<App />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>
 		</>
