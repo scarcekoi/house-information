2657b5bdb11240a35c0e56145cd9cf995ba4aa5d
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-06 19:39:46 -0500

    Login fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,3 +2,16 @@ import React from "react";
 import { Routes, Route } from "";
 import App from "./pages/App.tsx";
 import Login from "Lgn.tsx";
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />}>
+				<Route path="/login" element={<Login />}>
+			</Routes>
+		</>
+	);
+}
+
+export defaul App;
