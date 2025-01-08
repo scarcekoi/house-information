8ed7f769873076205785c97ee5c4ddc1916e1649
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-08 16:00:42 -0500

    Revert "Changes to pie chart and removed chart for now"
    This reverts commit b1a8d1844f1db2877323acfa43d6f2511957f5b9.

--- src/charts/totalhousepoints.tsx
+++ src/charts/totalhousepoints.tsx
@@ -15,7 +15,7 @@ const TotalHousePoints: React.FC<TotalHousePointsProps> = ({ counters }) => {
     const data = {
       labels: ['Baldwin', 'Sotomayor', 'Mandela', 'Truth'],
       datasets: [{
-        label: 'Total House Points Doughnut Chart',
+        label: 'Total House Points',
         data: [counters.Baldwin, counters.Sotomayor, counters.Mandela, counters.Truth],
         backgroundColor: [
           'rgb(210, 15, 57)',   // Baldwin color
@@ -55,7 +55,7 @@ const TotalHousePoints: React.FC<TotalHousePointsProps> = ({ counters }) => {
                 size: 24,
                 weight: 'normal',
               },
-              text: 'Total House Points',
+              text: 'Total House Points Doughnut Chart',
             },
           },
           elements: {
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css';
+import TotalHousePoints from '../charts/totalhousepoints';
 
 interface Counters {
   Baldwin: number;
