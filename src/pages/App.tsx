cf7a2c07203be60b2aca7940530cfb63ad1e14a0
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-08 07:03:39 -0500

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css';
 import TotalHousePoints from '../charts/totalhousepoints';
-import Footer from '../Footer';
 
 interface Counters {
   Baldwin: number;
