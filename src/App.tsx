ff14d6f7c85bec1bea345a45f1cb16d3a99f58f5
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-01-08 05:17:56 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import App from "./pages/App.tsx";
 import Login from "./pages/Lgn.tsx";
 
 function Index() {
   return (
-    <Router>
+    <>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
