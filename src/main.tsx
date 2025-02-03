a822133c79c342404292d0fc234bbb6697fafe46
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2025-02-02 20:12:20 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { useState, StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import Banner from "./Banner"
 
-const [currentHouse] = useState("Baldwin")
+const currentHouse = "Baldwin"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Banner currentHouse={currentHouse} />
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
